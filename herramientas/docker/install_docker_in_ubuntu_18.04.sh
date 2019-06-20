0ce4bda9fd0e38229883af7323cd60898ed6de4d
Erick Palacios Moreno <palmoreck@gmail.com>
2019-06-19 19:09:51 -0500

    adding shell script

---
+++ herramientas/docker/install_docker_in_ubuntu_18.04.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+##install docker for ubuntu:
+apt-get install -y apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+service docker start
